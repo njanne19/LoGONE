61d5e2a23155f9d95e757d279674939c2af1e93f
Brandon Apodaca <bapodaca@umich.edu>
2024-04-19 14:52:09 -0400

    still working on the dataloader

--- logone/dataloaders/logo_transform_dataset.py
+++ logone/dataloaders/logo_transform_dataset.py
@@ -1,6 +1,24 @@
 from torch.utils.data import Dataset
 import numpy as np
+import os
 
 class LogoTransformDataset(Dataset):
-    def __init__(self,annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = np.loadtxt(annotations_file)
+    def __init__(self,annotations_file, img_dir_t, img_dir_tt):
+        self.img_paths = np.genfromtxt(annotations_file,
+                                       delimiter=',', 
+                                       dtype=str, 
+                                       skip_header=1,
+                                       usecols=(0,1))
+        self.img_paths = np.genfromtxt(annotations_file,
+                                       delimiter=',', 
+                                       dtype=float, 
+                                       skip_header=1,
+                                       usecols=(0,1))
+        self.img_dir_t = img_dir_t
+        self.img_dir_tt = img_dir_tt
+
+    def __len__(self):
+        return len(os.listdir(self.img_dir_t))
+
+    def __getitem__(self, idx):
+        img_path = 
