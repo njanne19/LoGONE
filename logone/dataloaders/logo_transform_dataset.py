39e231b43fcb058a1380cac2d0f30eff848990e1
Brandon Apodaca <bapodaca@umich.edu>
2024-04-19 15:37:29 -0400

    testing dataloader

--- logone/dataloaders/logo_transform_dataset.py
+++ logone/dataloaders/logo_transform_dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 import numpy as np
 import os
+import cv2
 
 class LogoTransformDataset(Dataset):
     def __init__(self,annotations_file, img_dir_t, img_dir_tt):
@@ -9,7 +10,7 @@ class LogoTransformDataset(Dataset):
                                        dtype=str, 
                                        skip_header=1,
                                        usecols=(0,1))
-        self.img_paths = np.genfromtxt(annotations_file,
+        self.labels = np.genfromtxt(annotations_file,
                                        delimiter=',', 
                                        dtype=float, 
                                        skip_header=1,
@@ -21,4 +22,21 @@ class LogoTransformDataset(Dataset):
         return len(os.listdir(self.img_dir_t))
 
     def __getitem__(self, idx):
-        img_path = 
+        img_path_t = os.path.join(self.img_dir_t, self.img_paths[idx])
+        img_path_tt = os.path.join(self.img_dir_tt, self.img_paths[idx])
+        conc_img = np.concatenate((cv2.imread(img_path_t), cv2.imread(img_path_tt)), axis=2)
+        return conc_img, self.labels[idx]
+
+############# TESTING DATALOADER #############
+
+from torch.utils.data import DataLoader
+
+if __name__ == "__main__":
+    training_data = LogoTransformDataset(os.path.join(os.getcwd(), 'logone', 'utilities', 'labels.csv'), 
+                                         os.path.join(os.getcwd(), 'logone', 'utilities', 'transformed_256'), 
+                                         os.path.join(os.getcwd(), 'logone', 'utilities', 'transformed_transformed_256'))
+    test_data = LogoTransformDataset(os.path.join(os.getcwd(), 'logone', 'utilities', 'labels.csv'),
+                                     os.path.join(os.getcwd(), 'logone', 'utilities', 'transformed_256'),
+                                     os.path.join(os.getcwd(), 'logone', 'utilities', 'transformed_transformed_256'))
+    train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
